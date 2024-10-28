d45231de90bf20451c4c97e927d3907df1c50842
David Bong Xia Shing <davidbong@boostorder.com>
2024-10-29 06:58:37 +0800

    ✨: allow to get available collection more easily trade off for privacy

--- contracts/ElysiumTokenFactory.sol
+++ contracts/ElysiumTokenFactory.sol
@@ -8,10 +8,11 @@ import "./ElysiumToken.sol";
     Create new elysium token collection
 */
 /// @custom:security-contact davidbong@gmail.com
-contract ElysiumTokenFactory {
+contract ElysiumTokenFactory{
     // owner address => collection list
-    mapping(address => address[]) private _elysiumCollections;
-
+    address[] private _elysiumUsers;
+    address[] private _elysiumCollections;
+    mapping(address => address[]) private _elysiumUserCollections;
     mapping(address => bool) private _elysiumCollection;
 
     event CreatedCollection(
@@ -34,13 +35,75 @@ contract ElysiumTokenFactory {
             royalty,
             royaltyRecipient
         );
-        _elysiumCollections[msg.sender].push(address(token));
+        if (_elysiumUserCollections[msg.sender].length == 0) {
+            _elysiumUsers.push(msg.sender);
+        }
+        _elysiumUserCollections[msg.sender].push(address(token));
         _elysiumCollection[address(token)] = true;
+        _elysiumCollections.push(address(token));
         emit CreatedCollection(msg.sender, address(token), name, symbol);
     }
 
-    function getUserCollections() external view returns (address[] memory) {
-        return _elysiumCollections[msg.sender];
+    function getAllCollections() external view returns (address[][] memory) {
+        address[][] memory collections = new address[][](_elysiumUsers.length);
+
+        for (uint i = 0; i < _elysiumUsers.length; i++) {
+            collections[i] = _elysiumUserCollections[_elysiumUsers[i]];
+        }
+
+        return collections;
+    }
+
+    function getAllCollections(uint256 start,uint256 pagination) external view returns (address[] memory) {
+        uint256 totalCount = _elysiumCollections.length;
+        uint256 end = start + pagination;
+
+        if (start >= totalCount) {
+            return new address[](0);
+        }
+
+        if (end > totalCount) {
+            end = totalCount;
+        }
+
+        uint256 resultSize = end - start;
+        address[] memory result = new address[](resultSize);
+
+        for (uint256 i = 0; i < resultSize; i++) {
+            result[i] = _elysiumCollections[start + i];
+        }
+
+        return result;
+    }
+
+    function getAllUsers() external view returns (address[] memory) {
+        return _elysiumUsers;
+    }
+
+    function getAllUsers(uint256 start,uint256 pagination) external view returns (address[] memory) {
+        uint256 totalCount = _elysiumUsers.length;
+        uint256 end = start + pagination;
+
+        if (start >= totalCount) {
+            return new address[](0);
+        }
+
+        if (end > totalCount) {
+            end = totalCount;
+        }
+
+        uint256 resultSize = end - start;
+        address[] memory result = new address[](resultSize);
+
+        for (uint256 i = 0; i < resultSize; i++) {
+            result[i] = _elysiumUsers[start + i];
+        }
+
+        return result;
+    }
+
+    function getUserCollections(address userAddress) external view returns (address[] memory) {
+        return _elysiumUserCollections[userAddress];
     }
 
     function isElysiumCollection(address token) external view returns (bool) {
